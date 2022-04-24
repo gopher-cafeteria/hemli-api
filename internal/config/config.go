bcd55b3c6083fab29ef90009e93c9e69654938ec
eduard <eduard.hovhannisyan@pearson.com>
2022-04-24 12:56:11 +0200

    chore: add safe usage of env

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,38 +8,40 @@ import (
 )
 
 type Config struct {
+	Env  string
 	Host string
 	Port string
 }
 
 func NewConfig() *Config {
-	return &Config{}
+	return &Config{
+		Env: getenv("APP_ENV", "local"),
+	}
 }
 
 func (c *Config) Init() {
 	c.LoadEnv()
 
-	c.Host = os.Getenv("APP_HOST")
-	c.Port = os.Getenv("APP_PORT")
+	c.Host = getenv("APP_HOST", "localhost")
+	c.Port = getenv("APP_PORT", "8080")
 }
 
 func (c *Config) LoadEnv() {
-	env := os.Getenv("APP_ENV")
-	if env == "" {
-		env = "local"
-	}
+	filename := ".env." + c.Env
 
-	filename := ".env." + env
-	path := "../env/" + filename
-
-	if env == "production" {
-		path = "/etc/secrets/" + filename
-	}
-
-	err := godotenv.Load(path)
+	err := godotenv.Load("../env/" + filename)
 	if err != nil {
 		log.Fatalf("Error loading %s file", filename)
 	}
 
 	log.Printf("Loaded environment variables from %s", filename)
+}
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		log.Printf("%s environment variable not set. Using fallback: %s", key, fallback)
+		return fallback
+	}
+	return value
 }
